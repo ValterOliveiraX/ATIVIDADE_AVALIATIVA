0187df3423ae8a1b26864728313604781a291780
Valter Pereira Oliveira <95293263+ValterOliveiraX@users.noreply.github.com>
2024-08-04 16:44:46 -0300

    Adicionar arquivos de projeto.

---
+++ ATIVIDADE_AVALIATIVA/Form3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATIVIDADE_AVALIATIVA
+{
+    public partial class buscar : Form
+    {
+        public buscar()
+        {
+            InitializeComponent();
+        }
+
+        private void cadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ATIVIDADE_AVALIATIVA/Form6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATIVIDADE_AVALIATIVA
+{
+    public partial class cadastrar : Form
+    {
+        public cadastrar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
